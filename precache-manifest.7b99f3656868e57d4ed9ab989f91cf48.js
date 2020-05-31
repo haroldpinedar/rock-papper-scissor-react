c323ce972a3838a3e5700bf65b667250fe3f52f3
Harold-Pineda <hrpinedar91@gmail.com>
2020-05-30 21:27:03 -0500

    Updates

---
+++ precache-manifest.7b99f3656868e57d4ed9ab989f91cf48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c81bf686d56f6e398802c80393ed168",
+    "url": "/rock-papper-scissor-react/index.html"
+  },
+  {
+    "revision": "e9477d9984a1c57f6397",
+    "url": "/rock-papper-scissor-react/static/css/main.9900741b.chunk.css"
+  },
+  {
+    "revision": "4a2ba36ef127deab2e56",
+    "url": "/rock-papper-scissor-react/static/js/2.59fbafdd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rock-papper-scissor-react/static/js/2.59fbafdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9477d9984a1c57f6397",
+    "url": "/rock-papper-scissor-react/static/js/main.8ed2aabd.chunk.js"
+  },
+  {
+    "revision": "8f09f562d3f00e2d4238",
+    "url": "/rock-papper-scissor-react/static/js/runtime-main.8983697c.js"
+  }
+]);
